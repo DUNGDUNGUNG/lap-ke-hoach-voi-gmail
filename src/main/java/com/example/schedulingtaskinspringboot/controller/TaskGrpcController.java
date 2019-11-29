d2be6f3e555750076d45e7e6c11277be38fdb224
DUNGDUNGUNG <ledung276276@gmail.com>
2019-11-29 14:06:58 +0700

    update feedback array

--- src/main/java/com/example/schedulingtaskinspringboot/controller/TaskGrpcController.java
+++ src/main/java/com/example/schedulingtaskinspringboot/controller/TaskGrpcController.java
@@ -1,15 +1,15 @@
 package com.example.schedulingtaskinspringboot.controller;
 
-import com.example.grpc.EmailServiceGrpc;
+import com.example.grpc.*;
 
-import com.example.grpc.ListHistorySendEmailReq;
-import com.example.grpc.ListHistorySendEmailRes;
 import com.example.schedulingtaskinspringboot.model.HistorySendEmail;
 import com.example.schedulingtaskinspringboot.service.impl.HistorySendEmailServiceImpl;
 import io.grpc.stub.StreamObserver;
 import org.lognet.springboot.grpc.GRpcService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -22,16 +22,21 @@ public class TaskGrpcController extends EmailServiceGrpc.EmailServiceImplBase {
     @Override
     public void readAllHistorySendEmail(ListHistorySendEmailReq request, StreamObserver<ListHistorySendEmailRes> responseObserver) {
         List<HistorySendEmail> historySendEmails = sendEmailService.findAllHistory();
+        List<HistorySendEmailM> listItem = new ArrayList<>();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
         for (HistorySendEmail historySendEmail : historySendEmails) {
-            String message = "SendEmail: " + historySendEmail.getSendEmail().getUsername() +
-                    ", Title: " + historySendEmail.getTitle() +
-                    ", Status: " + historySendEmail.getStatus() +
-                    ", ReceiveEmail: " + historySendEmail.getReceiveEmail().getEmail() +
-                    ", Time: " + historySendEmail.getTime();
-
-            ListHistorySendEmailRes listHistorySendEmailRes = ListHistorySendEmailRes.newBuilder().setMessage(message).build();
-            responseObserver.onNext(listHistorySendEmailRes);
+            HistorySendEmailM item = HistorySendEmailM.newBuilder().setId(historySendEmail.getId())
+                    .setSendEmailM(SendEmailM.newBuilder().setUsername(historySendEmail.getSendEmail().getUsername()).build())
+                    .setTitle(historySendEmail.getTitle())
+                    .setStatus(historySendEmail.getStatus())
+                    .setReceiveEmail(ReceiveEmailM.newBuilder().setEmail(historySendEmail.getReceiveEmail().getEmail()).build())
+                    .setTime(historySendEmail.getTime().format(dateTimeFormatter))
+                    .build();
+            listItem.add(item);
         }
+
+        ListHistorySendEmailRes listHistorySendEmailRes = ListHistorySendEmailRes.newBuilder().addAllHistorySendEmailM(listItem).build();
+        responseObserver.onNext(listHistorySendEmailRes);
 
 
         responseObserver.onCompleted();
